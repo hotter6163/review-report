df0f616f9bb8e21256a4fa21893ff3dc543af893
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-11-04 15:13:58 +0900

    :sparkles: prismaとnext-authを導入

---
+++ src/constants/authOptions.ts
@@ -0,0 +1,23 @@
+import prisma from "@/libs/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+};
