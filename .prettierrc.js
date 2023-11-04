8477edcef8a924d7e6cda723ccfb8114617152d3
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-11-04 15:38:10 +0900

    :wrench: prettierを設定

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss', '@trivago/prettier-plugin-sort-imports'],
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
